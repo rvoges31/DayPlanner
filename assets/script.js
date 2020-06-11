c44b0f530d2292396043fa6491284ad3247518e5
rvoges31 <59306580+rvoges31@users.noreply.github.com>
2020-06-11 20:15:18 +0800

    updates to script to get text working

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,15 @@
 
 var workHrs = [7,8,9,10,11,12,13,14,15,16,17,18];
 
+var currentDate = moment().format("dddd, MMMM Do YYYY");
+$(".date").html(currentDate);
 var currentHr = moment().hour();
 
 function startPlanner () {
     for (i = 0 ; i < workHrs.length ; i++) {
         var getText = localStorage.getItem("textA" + i);
         var row = $("<div class = 'row'>");
-        var col1 = $("<div class = 'col-sm-3 text-left'>");
+        var col1 = $("<div class = 'col-lg-1 text-left'>");
         var midday = " AM";
         if (workHrs[i]> 12) {
             midday = " PM";
@@ -19,26 +21,26 @@ function startPlanner () {
         }
         col1.html(normalHrs + midday);
 
-        var col2 = $("<div class = 'col-sm-6'>");
+        var col2 = $("<div class = 'col-lg-10'>");
 
-        var textA = $("<text style='width:100%'>");
+        var textA = $("<p>");
         textA.attr("id", "textA" + i);
         // Define colour of hours current before and after, current is green, past is grey, future is yellow
-        var color = "bg-success";
+        var colour = "bg-success";
 
         if (workHrs[i] > currentHr) {
-            color = "bg-warning";
+            colour = "bg-warning";
         }
         
         if (workHrs[i] < currentHr) {
-            color = "bg-info";
+            colour = "bg-info";
         }
 
-        textA.attr("class", color);
+        textA.attr("class", colour);
         textA.val(getText);
         col2.append(textA);
 
-        var col3 = $("<div class = 'col-sm-3'>");
+        var col3 = $("<div class = 'col-lg-1'>");
         var btn = $("<button class='btn btn-dark save'>");
         btn.text("Save");
         col3.append(btn);
@@ -53,7 +55,8 @@ function startPlanner () {
 startPlanner();
 
 $(".save").on("click", function() {
-    for (i = 0; i < workHrs.length; i++);
-    var textInput = $("#textA" + i).val();
-    localStorage.setItem("textA" + i, textInput);
-})
+    for (i = 0; i < workHrs.length; i++) {
+        var textInput = $("#textA" + i).val();
+        localStorage.setItem("textA" + i, textInput);
+    }
+});
