6ec6eeb5906ecba908cbea9db872003a61b3c2f4
rvoges31 <59306580+rvoges31@users.noreply.github.com>
2020-06-13 11:57:01 +0800

    Added nonmenclature to script

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-//define variables
+//declare variables
 var appointment = "";
 var scheduleTime = "";
 var currentDate;
@@ -8,13 +8,16 @@ var tempArray = [];
 var returnedToDos;
 var returnedAppointments;
 
-
+// Only run function after page has loaded
 $(window).on("load", function() {
+    // Get current date and current time from moment.js
     currentDate = moment().format("dddd MMM Do YYYY");
+    //Set Date in HTML
     $("#date").append(currentDate);
     currentTime = moment().format("H");
 
     function renderAppointments() {
+        //Get object data from local storage - JSON.parse
         returnedToDos = JSON.parse(localStorage.getItem("appointments"));
         if (returnedToDos !== null) {
             for (i=0;i<returnedToDos.length;i++) {
@@ -29,9 +32,9 @@ $(window).on("load", function() {
             }
         }
     }
-
+    // Run function 
     renderAppointments()
-
+    //Set colours for present, past and future - add class and style in css
     for (i = 0; i < 24; i++) {
         CurrentC = i;
         if (currentTime == i) {
@@ -47,24 +50,29 @@ $(window).on("load", function() {
             $('#' + CurrentC).children('div').children('div').children("textarea").addClass("future");
         }
     }
-})
+});
 
 $(".saveBtn").click(function () {
     appointment = $(this).parent('div').children('div').children('textarea').val();
     scheduleTime = $(this).parent('div').parent().attr("id");
+    // Creat object to hold appointment details and respective time
     appointment = {
         time: scheduleTime,
         details: appointment
     }
+    // Get temporary array of appointmenets (if existing)
     tempArray = JSON.parse(localStorage.getItem("appointments"));
     if (tempArray === null) {
+        // Store apointment to locat storage
         localStorage.setItem('appointments', JSON.stringify([{ time: scheduleTime, details: appointment }]));
     }
+    // Set temporar array of appointments (if not yet existing)
     else {
         tempArray.push(appointment);
         localStorage.setItem("appointments", JSON.stringify(tempArray));
 
     }
+    // Replace text area with appointment
     $(this).parent('div').children('div').children('textarea').replaceWith($('<textarea>' + appointment.addClass("textarea") + '</textarea>'));
 });
 
